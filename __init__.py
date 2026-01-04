45071cb0d2f8a463896d79bfb2d951b68ecaa071
Taithrah <feralfox@verizon.net>
2026-01-03 21:21:10 -0500

    updated to V3 schema. depreciated older version. Updated ComfyUI required.

--- __init__.py
+++ __init__.py
@@ -1,29 +1,21 @@
 from comfy_api.latest import ComfyExtension, io
+from typing_extensions import override
 
 from .nodes.FensTokenCounter import FensTokenCounter
 from .nodes.OptiEmptyLatent import OptiEmptyLatent
 from .nodes.OptiEmptyLatentAdvanced import OptiEmptyLatentAdvanced
 
-NODE_CLASS_MAPPINGS = {
-    "FensTokenCounter": FensTokenCounter,
-    "OptiEmptyLatent": OptiEmptyLatent,
-    "OptiEmptyLatentAdvanced": OptiEmptyLatentAdvanced,
-}
-
-NODE_DISPLAY_NAME_MAPPINGS = {
-    "FensTokenCounter": "Fens Token Counter",
-    "OptiEmptyLatent": "Optimal Empty Latent",
-    "OptiEmptyLatentAdvanced": "Optimal Empty Latent (Advanced)",
-}
-
 WEB_DIRECTORY = "./web"
-__all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
 
 
 class FensSimpleNodesExtension(ComfyExtension):
+    @override
     async def get_node_list(self) -> list[type[io.ComfyNode]]:
         return [FensTokenCounter, OptiEmptyLatent, OptiEmptyLatentAdvanced]
 
 
 async def comfy_entrypoint() -> FensSimpleNodesExtension:
     return FensSimpleNodesExtension()
+
+
+__all__ = ["comfy_entrypoint", "WEB_DIRECTORY"]
